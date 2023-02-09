7389a3c35afa2f0a018102c2bb4d537744795bd4
shaikmohammedabdullah <smohammedabdullah77@gmail.com>
2023-02-09 05:28:28 +0000

    welcome app

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,44 @@
+import {Component} from 'react'
+import './index.css'
+
+class Welcome extends Component {
+  state = {isSubscribed: true}
+
+  subscriptionOn = () => {
+    this.setState(() => ({isSubscribed: false}))
+  }
+
+  subscriptionOff = () => {
+    this.setState(() => ({isSubscribed: true}))
+  }
+
+  render() {
+    const {isSubscribed} = this.state
+
+    return (
+      <div className="container">
+        <h1 className="welcome-title">Welcome</h1>
+        <p className="description">Thank you! Happy Learning</p>
+        {isSubscribed ? (
+          <button
+            className="button"
+            type="button"
+            onClick={this.subscriptionOn}
+          >
+            Subscribe
+          </button>
+        ) : (
+          <button
+            className="button"
+            type="button"
+            onClick={this.subscriptionOff}
+          >
+            Subscribed
+          </button>
+        )}
+      </div>
+    )
+  }
+}
+
+export default Welcome
